8c960ccf326e1541738cb6ea8a3ca8c5db6336c8
wuihee <wuihee@gmail.com>
2023-11-07 06:57:38 -0800

    Updated basicMondrian

--- creative_project_2/src/Mondrian.java
+++ creative_project_2/src/Mondrian.java
@@ -32,32 +32,35 @@ public class Mondrian {
      * Recursive auxilliary method to paint the Mondrian.
      * 
      * @param pixels Matrix of Color objects that represent the canvas.
-     * @param r The starting row of the current section.
-     * @param c The starting column of the current section
-     * @param width The width of the current section.
-     * @param height The height of the current section.
+     * @param w1 The starting column of the current section.
+     * @param h1 The starting row of the current section
+     * @param w2 The ending column of the current section.
+     * @param h2 The ending row of the current section.
      */
     private void paintBasicMondrian(Color[][] pixels, int w1, int h1, int w2, int h2) {
         int fullHeight = pixels.length;
         int fullWidth = pixels[0].length;
         int height = h2 - h1;
         int width = w2 - w1;
-        int splitWidth = w1 + random.nextInt(width);
-        int splitHeight = h1 + random.nextInt(height);
 
-        if (height >= fullHeight / 4 && width >= fullWidth / 4) {
-            paintBasicMondrian(pixels, w1, h1, splitWidth, splitHeight);
-            paintBasicMondrian(pixels, splitWidth + 1, h1, w2, splitHeight);
-            paintBasicMondrian(pixels, w1, splitHeight + 1, splitWidth, h2);
-            paintBasicMondrian(pixels, splitWidth + 1, splitHeight + 1, w2, h2);
-        } else if (height >= fullHeight / 4) {
-            paintBasicMondrian(pixels, w1, h1, w2, splitHeight);
-            paintBasicMondrian(pixels, w1, splitHeight + 1, w2, h2);
-        } else if (width >= fullWidth / 4) {
-            paintBasicMondrian(pixels, w1, h1, splitWidth, h2);
-            paintBasicMondrian(pixels, splitWidth + 1, h1, w2, h2); 
-        } else {
-            fillRegion(pixels, w1, h1, w2, h2);
+        if (width != 0 && height != 0) {
+            int splitWidth = w1 + random.nextInt(width);
+            int splitHeight = h1 + random.nextInt(height);
+
+            if (height >= fullHeight / 4 && width >= fullWidth / 4) {
+                paintBasicMondrian(pixels, w1, h1, splitWidth, splitHeight);
+                paintBasicMondrian(pixels, splitWidth + 1, h1, w2, splitHeight);
+                paintBasicMondrian(pixels, w1, splitHeight + 1, splitWidth, h2);
+                paintBasicMondrian(pixels, splitWidth + 1, splitHeight + 1, w2, h2);
+            } else if (height >= fullHeight / 4) {
+                paintBasicMondrian(pixels, w1, h1, w2, splitHeight);
+                paintBasicMondrian(pixels, w1, splitHeight + 1, w2, h2);
+            } else if (width >= fullWidth / 4) {
+                paintBasicMondrian(pixels, w1, h1, splitWidth, h2);
+                paintBasicMondrian(pixels, splitWidth + 1, h1, w2, h2); 
+            } else {
+                fillRegion(pixels, w1, h1, w2, h2);
+            }
         }
     }
 
@@ -65,10 +68,10 @@ public class Mondrian {
      * Auxilliary method to fill a specified region with a random Mondrian color.
      * 
      * @param pixels Matrix of Color objects that represent the canvas.
-     * @param r The starting row of the current section.
-     * @param c The starting column of the current section.
-     * @param height The height of the current section.
-     * @param width The width of the current section.
+     * @param w1 The starting column of the current section.
+     * @param h1 The starting row of the current section
+     * @param w2 The ending column of the current section.
+     * @param h2 The ending row of the current section.
      */
     private void fillRegion(Color[][] pixels, int w1, int h1, int w2, int h2) {
         Color color = mondrianColors[random.nextInt(mondrianColors.length)];
